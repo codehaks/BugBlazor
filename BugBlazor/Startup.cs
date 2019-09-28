49ec1f6701d83b7cd1c00ff3e7b9565f574b235a
Codehaks <ghods.hakim@gmail.com>
2019-09-28 05:36:02 +0330

    Bugs database

--- BugBlazor/Startup.cs
+++ BugBlazor/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BugBlazor.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +18,9 @@ namespace BugBlazor
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BugDb>(options =>
+             options.UseSqlite("DataSource=bug.sqlite"));
+
             services.AddRazorPages()
                 .AddRazorRuntimeCompilation();
             services.AddServerSideBlazor();

a0810cd8a07698179f35ad256a2ee348a88d4546
Codehaks <ghods.hakim@gmail.com>
2019-09-28 05:09:58 +0330

    Add project files.

---
+++ BugBlazor/Models/Bug.cs
@@ -0,0 +1,9 @@
+namespace BugBlazor.Models
+{
+    public class Bug
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}

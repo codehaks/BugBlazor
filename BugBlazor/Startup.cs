7cb3be69e83f5d6d632296377c639bc333ca2d9a
Codehaks <ghods.hakim@gmail.com>
2019-09-28 05:17:05 +0330

    Runtime Compilation

--- BugBlazor/Startup.cs
+++ BugBlazor/Startup.cs
@@ -16,7 +16,8 @@ namespace BugBlazor
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages()
+                .AddRazorRuntimeCompilation();
             services.AddServerSideBlazor();
         }
 
